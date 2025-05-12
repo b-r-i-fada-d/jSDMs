02cbd0d9f1ab53a80006ace2aaeed09cab9a1780
U-INSTITUTE\boconnor <boconnor@MI6925.Marine.ie>
2025-05-12 13:20:40 +0100

    File 3 for 800 samples.

---
+++ 03_compute_model_fit_800.R
@@ -0,0 +1,43 @@
+# --- Set up --- ####
+
+library("Hmsc")
+library("tidyverse")
+
+localDir = "."
+data.directory = file.path(localDir, "data")
+model.directory = file.path(localDir, "models")
+results.directory = file.path(localDir, "results")
+
+set.seed(13)
+
+# --- SETTING COMMONLY ADJUSTED PARAMETERS TO NULL WHICH CORRESPONDS TO DEFAULT CHOICE --- ####
+
+filename = "models/m_FULL_800.RData"
+load(filename)
+model <- m_FULL_800 # EDIT
+rm(m_FULL_800)
+
+thin = 1
+samples = 800
+nChains = 4
+nfolds = 1 # Default: two-fold cross-validation
+# cv.level = "year"
+nParallel = 12 # Default: nParallel = nChains
+ 
+
+preds = computePredictedValues(model)
+MF = evaluateModelFit(hM = model, predY = preds)
+partition = createPartition(model, nfolds = nfolds
+                            # , column = cv.level
+                            )
+
+predsCV = computePredictedValues(model,
+                                 partition = partition,
+                                 nParallel = nParallel
+                                 )
+MFCV = evaluateModelFit(hM = model, predY = predsCV)
+WAIC = computeWAIC(model)
+  
+save(MF, 
+     MFCV, 
+     WAIC, file = "results/model_fit_FULL_800.Rdata")
