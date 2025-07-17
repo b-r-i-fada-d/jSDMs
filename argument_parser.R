616bfa99e2a38c31e63fb4b835835d102f94a6c9
U-INSTITUTE\boconnor <boconnor@MI6925.Marine.ie>
2025-07-17 01:10:33 +0100

    Adding new version of argument parser.

--- argument_parser.R
+++ argument_parser.R
@@ -1,10 +1,20 @@
-library(argparse)
+library("argparse")
 
-parser <- ArgumentParser(description='HMSC')
-parser$add_argument("-s", "--n_samples", type = "integer", required=TRUE)
-parser$add_argument("-c", "--n_chains", type = "integer", required=TRUE)
-parser$add_argument("-t", "--n_thins", type = "integer", required=TRUE)
-parser$add_argument("-r", "--n_transients", type = "integer", required=TRUE)
+parser <- ArgumentParser(description='HMSC-HPC Pipeline')
+parser$add_argument("-i", "--model-rds", type = "character", required=TRUE)
+parser$add_argument("-o", "--output-dir", type = "character", required=TRUE)
 parser$add_argument("-m", "--model", type = "character", required=TRUE)
 
+parser$add_argument("-s", "--n-samples", type = "integer", required=TRUE)
+parser$add_argument("-c", "--n-chains", type = "integer", required=TRUE)
+parser$add_argument("-t", "--n-thins", type = "integer", required=TRUE)
+parser$add_argument("-r", "--n-transients", type = "integer", required=TRUE)
+
+parser$add_argument("-f", "--species-data", type = "character", required=TRUE)
+parser$add_argument("-e", "--env-data", type = "character", required=TRUE)
+
+# parser$add_argument("-x", "--seed", type = "integer", required=FALSE)
+parser$add_argument("-v", "--verbosity", type = "integer", required=FALSE)
+
 arguments <- parser$parse_args()
+print(arguments)
