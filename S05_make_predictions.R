a3c50be76808a3ad439265452129145637571e97
Bríd O'Connor <b.oconnor92@gmail.com>
2025-09-16 10:48:34 +0100

    Removing RLs

--- S05_make_predictions.R
+++ S05_make_predictions.R
@@ -162,7 +162,7 @@ ranLevels = list(station = rL.station,
 predY <- predict(model,
                  XData = XData,
                  studyDesign = studyDesign,
-                 ranLevels = list(),
+                 # ranLevels = list(),
                  predictEtaMean = TRUE,
                  expected = TRUE
 )
